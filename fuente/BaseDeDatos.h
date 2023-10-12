9997466bb71cfa9d49c50324184f9e96e5b0aeea
Enrique134 <136546184+Enrique134@users.noreply.github.com>
2023-10-12 11:40:01 -0600

    Update BaseDeDatos.h

--- fuente/BaseDeDatos.h
+++ fuente/BaseDeDatos.h
@@ -7,6 +7,9 @@
 using namespace std;
 
 // contenedor para las ubicaciónes de los archivos de almacenamiento
+
+BaseDePatiosParaLosDatos
+
 map<string, string> BaseDeDatos = {
   {"DatosDeContactos", "datos/datos_de_contactos"}
 };
@@ -18,7 +21,7 @@ enum Almacenes = {
 };
 
 template T<Clase>
-void guardar(Almacenes almacen, Clase datos) {
+void guardar(Almacenes almacen, string identificador, Clase datos) {
   string ubicación; // ubicación para el archivo de almacenamiento de datos
 
   switch(almacen)
@@ -26,12 +29,16 @@ void guardar(Almacenes almacen, Clase datos) {
     case Almacenes.DatosDeContactos:
       ubicación = BaseDeDatos.at("DatosDeContactos");
       break;
-
+    
     default:
       break;
   }
 
-  FILE* archivo = fopen(ubicación);
+  FILE* archivo = fopen(ubicación, "a");
+  fwrite(datos)
+}
+
+void actualizar(string identificador) {
   
 }
 
